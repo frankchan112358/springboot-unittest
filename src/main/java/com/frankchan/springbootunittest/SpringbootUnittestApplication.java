8985b5f42e62835f95e40f5eba1a172f40339b29
Frank Chan <frank.chan@hy-tech.com.tw>
2022-04-17 11:42:07 +0800

    初始化 Spring Boot Unit Test Project

---
+++ src/main/java/com/frankchan/springbootunittest/SpringbootUnittestApplication.java
@@ -0,0 +1,13 @@
+package com.frankchan.springbootunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUnittestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUnittestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frankchan/springbootunittest/SpringbootUnittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankchan.springbootunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUnittestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
